0c0a03c06a7a556cb0a2c95b2299e0ee6f663f4c
Petrol95 <petrol195@yandex.ru>
2018-11-22 02:57:32 +0300

    enum AppState created

--- src/Lesson_1/MainClass.java
+++ src/Lesson_1/MainClass.java
@@ -2,13 +2,31 @@ package Lesson_1;
 
 public class MainClass {
 
+//    static final int APP_START = 0;
+//    static final int APP_PREPARE = 1;
+//    static final int APP_WORK = 2;
+//    static final int APP_SAVE_DATA = 3;
+
+    static AppState appState = AppState.INIT;
+
     public static void main(String[] args) {
-        Animal a = new Cat();
-        Flyable f = new Parrot();
-        f.fly();
-        f.defaultFly();
-        ((Parrot)f).voice();
-        Predator[] predators = {new Cat(), new Cat(), new Cat()};
+
+//        appState++;
+        System.out.println(appState.getPriority());
+        appState = AppState.WORK;
+        System.out.println(appState.getPriority());
 
     }
+
+//    public static void main(String[] args) {
+//
+////        Animal a = new Cat();
+////        Flyable f = new Parrot();
+////        f.fly();
+////        f.defaultFly();
+////        ((Parrot)f).voice();
+////        Predator[] predators = {new Cat(), new Cat(), new Cat()};
+//
+//    }
+
 }
