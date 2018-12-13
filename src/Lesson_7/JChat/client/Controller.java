6c4e2cf51d2a703fad7e308d986bab27ea80e965
Petrol95 <petrol195@yandex.ru>
2018-12-13 17:03:56 +0300

    implemented connection to DB, improvements

--- src/Lesson_7/JChat/client/Controller.java
+++ src/Lesson_7/JChat/client/Controller.java
@@ -1,9 +1,13 @@
 package Lesson_7.JChat.client;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -15,9 +19,16 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable{
     @FXML
     TextArea textArea;
-
     @FXML
     TextField msgField;
+    @FXML
+    HBox authPanel;
+    @FXML
+    HBox msgPanel;
+    @FXML
+    TextField loginField;
+    @FXML
+    PasswordField passField;
 
     private Socket socket;
     private DataOutputStream out;
@@ -26,34 +37,76 @@ public class Controller implements Initializable{
     final String SERVER_IP = "localhost";
     final int SERVER_PORT = 8189;
 
+    private boolean authorized;
+
+    public void setAuthorized(boolean authorized) {
+        this.authorized = authorized;
+        if (authorized) {
+            msgPanel.setVisible(true);
+            msgPanel.setManaged(true);
+            authPanel.setVisible(false);
+            authPanel.setManaged(false);
+        } else {
+            msgPanel.setVisible(false);
+            msgPanel.setManaged(false);
+            authPanel.setVisible(true);
+            authPanel.setManaged(true);
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        setAuthorized(false);
+    }
+
+    public void connect() {
         try {
             socket = new Socket(SERVER_IP, SERVER_PORT);
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
+
             Thread t = new Thread(() -> {
                 try {
+                    while (true) {
+                        String s = in.readUTF();
+                        if (s.equals("/authok")) {
+                            setAuthorized(true);
+                            break;
+                        }
+                        textArea.appendText(s + "\n");
+                    }
                     while (true) {
                         String s = in.readUTF();
                         textArea.appendText(s + "\n");
                     }
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    showAlert("Сервер перестал отвечать");
                 } finally {
+                    setAuthorized(false);
                     try {
                         socket.close();
                     } catch (IOException e) {
-                        e.printStackTrace();
+                        showAlert("Не удалось подключиться к серверу. Проверьте сетевое соединение");
                     }
                 }
-
-
             });
             t.setDaemon(true);
             t.start();
         } catch (IOException e) {
-            e.printStackTrace();
+            showAlert("Не удалось подключиться к серверу. Проверьте сетевое соединение");
+        }
+    }
+
+    public void sendAuthMsg() {
+        if ((socket == null) || (socket.isClosed())) {
+            connect();
+        }
+        try { // "/auth login pass"
+            out.writeUTF("/auth " + loginField.getText() + " " + passField.getText());
+            loginField.clear();
+            passField.clear();
+        } catch (IOException e) {
+            showAlert("Не удалось подключиться к серверу. Проверьте сетевое соединение");
         }
     }
 
@@ -63,9 +116,17 @@ public class Controller implements Initializable{
             msgField.clear();
             msgField.requestFocus();
         } catch (IOException e) {
-            e.printStackTrace();
+            showAlert("Не удалось подключиться к серверу. Проверьте сетевое соединение");
         }
     }
 
-
+    public void showAlert(String msg){
+        Platform.runLater(() -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Возникли проблемы");
+            alert.setHeaderText(null);
+            alert.setContentText(msg);
+            alert.showAndWait();
+        });
+    }
 }
--- src/Lesson_7/JChat/server/ClientHandler.java
+++ src/Lesson_7/JChat/server/ClientHandler.java
@@ -1,5 +1,8 @@
 package Lesson_7.JChat.server;
 
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -10,6 +13,11 @@ public class ClientHandler {
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
+    private String nick;
+
+    public String getNick() {
+        return nick;
+    }
 
     public ClientHandler(Server server, Socket socket) {
         try {
@@ -17,27 +25,46 @@ public class ClientHandler {
             this.socket = socket;
             this.in = new DataInputStream(socket.getInputStream());
             this.out = new DataOutputStream(socket.getOutputStream());
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try{
-                        while(true){
-                            String msg = in.readUTF();
-                            System.out.println("client: " + msg);
-                            server.broadcastMsg(msg);
-//                            sendMsg("echo: " + msg);
-                            if (msg.equals("/end")) break;
-                        }
-                    }catch (IOException e){
-                        e.printStackTrace();
-                    }finally {
-                        try {
-                            socket.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
+            new Thread(() -> {
+                try {
+                    while(true) {
+                        String msg = in.readUTF();
+                        if (msg.startsWith("/auth ")) {
+                            String[] data = msg.split("\\s");
+                            String newNick = server.getAuthService().getNickByLoginAndPass(data[1], data[2]);
+                            if (newNick != null) {
+                                if (!server.isNickBusy(newNick)) {
+                                    nick = newNick;
+                                    sendMsg("/authok");
+                                    server.subscribe(this);
+                                    break;
+                                } else {
+                                    sendMsg("Учетная запись уже занята");
+                                }
+                            } else {
+                                sendMsg("Неверный логин/пароль");
+                            }
                         }
                     }
-
+                    while(true) {
+                        String msg = in.readUTF();
+                        System.out.println(nick + ": " + msg);
+                        if (msg.startsWith("/")) {
+                            if (msg.equals("/end")) break;
+                        } else {
+                            server.broadcastMsg(nick + ": " + msg);
+                        }
+                    }
+                } catch (IOException e){
+                    e.printStackTrace();
+                } finally {
+                    nick = null;
+                    server.unsubscribe(this);
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }).start();
 
@@ -52,6 +79,16 @@ public class ClientHandler {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void showAlert(String msg){
+        Platform.runLater(() -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Возникли проблемы");
+            alert.setHeaderText(null);
+            alert.setContentText(msg);
+            alert.showAndWait();
+        });
     }
 
 }
